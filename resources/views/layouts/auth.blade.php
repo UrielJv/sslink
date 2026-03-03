18cc499f7cbdb07ba6499ab36db79cc06021df25
Uriel Jv <alfredo010505@gmail.com>
2026-03-03 14:39:00 -0600

    puto si no es esta

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,33 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Iniciar sesión')</title>
 
-    <link rel="icon" href="{{ asset('auth-assets/images/logo-sm.png') }}" type="image/png">
+    <link rel="icon" href="{{ secure_asset('auth-assets/images/logo-sm.png') }}" type="image/png">
 
     {{-- Librerías --}}
-    <link rel="stylesheet" href="{{ asset('auth-lib/bootstrap_5/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('auth-lib/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('auth-lib/bootstrap_5/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('auth-lib/fontawesome/css/all.min.css') }}">
 
     {{-- CSS del login --}}
-    <link rel="stylesheet" href="{{ asset('auth-assets/css/global.css') }}">
-    <link rel="stylesheet" href="{{ asset('auth-assets/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('auth-assets/css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('auth-assets/css/global.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('auth-assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('auth-assets/css/responsive.css') }}">
 
     @stack('styles')
 </head>
 
 <body class="d2c_theme_light" style="background-color: #344767">
 
-    {{-- Preloader --}}
-    {{-- <div class="preloader">
-        <img src="{{ asset('auth-assets/images/logo.png') }}" alt="Logo">
-    </div> --}}
-
     @yield('content')
 
     {{-- JS (orden correcto) --}}
-    <script src="{{ asset('auth-lib/jQuery/jquery-3.5.1.min.js') }}"></script>
-    <script src="{{ asset('auth-lib/bootstrap_5/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('auth-assets/js/main.js') }}"></script>
+    <script src="{{ secure_asset('auth-lib/jQuery/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ secure_asset('auth-lib/bootstrap_5/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('auth-assets/js/main.js') }}"></script>
 
     @stack('scripts')
 </body>
